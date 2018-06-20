c2f423b0face17164f1bfa0958f8067d325b5e97
Karl McGuire <karl@xba.io>
2018-06-20 00:11:57 -0400

    typo

--- decrypt.go
+++ decrypt.go
@@ -4,31 +4,31 @@ import (
 	"crypto/cipher"
 )
 
-// NewIGEDecrypter returns an IGE cipher.BlockMode which decrypts using IGE and
+// NewIGEDecrypter returns an IGE cipher.BlockMode which encrypts using IGE and
 // the given cipher.Block.
 //
 // Note: iv must contain two iv values for IGE (concatenated), otherwise this
 // function will panic. See ErrInvalidIV for more information.
-func NewIGEEncrypter(b cipher.Block, iv []byte) IGE {
+func NewIGEDecrypter(b cipher.Block, iv []byte) IGE {
 	if err := checkIV(b, iv); err != nil {
 		panic(err.Error())
 	}
 
-	return (*igeEncrypter)(newIGE(b, iv))
+	return (*igeDecrypter)(newIGE(b, iv))
 }
 
-type igeEncrypter ige
+type igeDecrypter ige
 
-func (i *igeEncrypter) BlockSize() int {
+func (i *igeDecrypter) BlockSize() int {
 	return i.block.BlockSize()
 }
 
-func (i *igeEncrypter) CryptBlocks(dst, src []byte) {
+func (i *igeDecrypter) CryptBlocks(dst, src []byte) {
 	if len(src)%i.block.BlockSize() != 0 {
 		panic("src not full blocks")
 	}
 	if len(dst) < len(src) {
-		panic("len(dst) < len(src")
+		panic("len(dst) < len(src)")
 	}
 
 	b := i.block.BlockSize()
@@ -36,11 +36,13 @@ func (i *igeEncrypter) CryptBlocks(dst, src []byte) {
 	m := i.iv[b:]
 
 	for o := 0; o < len(src); o += b {
-		xor(dst[o:o+b], src[o:o+b], c)
-		i.block.Encrypt(dst[o:o+b], dst[o:o+b])
-		xor(dst[o:o+b], dst[o:o+b], m)
+		t := src[o : o+b]
 
-		c = dst[o : o+b]
-		m = src[o : o+b]
+		xor(dst[o:o+b], src[o:o+b], m)
+		i.block.Decrypt(dst[o:o+b], dst[o:o+b])
+		xor(dst[o:o+b], dst[o:o+b], c)
+
+		m = dst[o : o+b]
+		c = t
 	}
 }
--- encrypt.go
+++ encrypt.go
@@ -4,31 +4,31 @@ import (
 	"crypto/cipher"
 )
 
-// NewIGEDecrypter returns an IGE cipher.BlockMode which encrypts using IGE and
+// NewIGEEncrypter returns an IGE cipher.BlockMode which encrypts using IGE and
 // the given cipher.Block.
 //
 // Note: iv must contain two iv values for IGE (concatenated), otherwise this
 // function will panic. See ErrInvalidIV for more information.
-func NewIGEDecrypter(b cipher.Block, iv []byte) IGE {
+func NewIGEEncrypter(b cipher.Block, iv []byte) IGE {
 	if err := checkIV(b, iv); err != nil {
 		panic(err.Error())
 	}
 
-	return (*igeDecrypter)(newIGE(b, iv))
+	return (*igeEncrypter)(newIGE(b, iv))
 }
 
-type igeDecrypter ige
+type igeEncrypter ige
 
-func (i *igeDecrypter) BlockSize() int {
+func (i *igeEncrypter) BlockSize() int {
 	return i.block.BlockSize()
 }
 
-func (i *igeDecrypter) CryptBlocks(dst, src []byte) {
+func (i *igeEncrypter) CryptBlocks(dst, src []byte) {
 	if len(src)%i.block.BlockSize() != 0 {
 		panic("src not full blocks")
 	}
 	if len(dst) < len(src) {
-		panic("len(dst) < len(src)")
+		panic("len(dst) < len(src")
 	}
 
 	b := i.block.BlockSize()
@@ -36,13 +36,11 @@ func (i *igeDecrypter) CryptBlocks(dst, src []byte) {
 	m := i.iv[b:]
 
 	for o := 0; o < len(src); o += b {
-		t := src[o : o+b]
+		xor(dst[o:o+b], src[o:o+b], c)
+		i.block.Encrypt(dst[o:o+b], dst[o:o+b])
+		xor(dst[o:o+b], dst[o:o+b], m)
 
-		xor(dst[o:o+b], src[o:o+b], m)
-		i.block.Decrypt(dst[o:o+b], dst[o:o+b])
-		xor(dst[o:o+b], dst[o:o+b], c)
-
-		m = dst[o : o+b]
-		c = t
+		c = dst[o : o+b]
+		m = src[o : o+b]
 	}
 }
