26c9656e95aa69551f907db1c54f282095c5672d
Karl McGuire <karl@xba.io>
2018-06-19 23:45:41 -0400

    separate files

---
+++ encrypt.go
@@ -0,0 +1,44 @@
+package ige
+
+import (
+	"crypto/cipher"
+)
+
+func NewIGEDecrypter(b cipher.Block, iv []byte) IGE {
+	if err := checkIV(b, iv); err != nil {
+		panic(err.Error())
+	}
+
+	return (*igeDecrypter)(newIGE(b, iv))
+}
+
+type igeDecrypter ige
+
+func (i *igeDecrypter) BlockSize() int {
+	return i.block.BlockSize()
+}
+
+func (i *igeDecrypter) CryptBlocks(dst, src []byte) {
+	if len(src)%i.block.BlockSize() != 0 {
+		panic("src not full blocks")
+	}
+	if len(dst) < len(src) {
+		panic("len(dst) < len(src)")
+	}
+
+	b := i.block.BlockSize()
+	c := i.iv[:b]
+	m := i.iv[b:]
+	t := make([]byte, b)
+
+	for o := 0; o < len(src); o += b {
+		t = src[o : o+b]
+
+		xor(dst[o:o+b], src[o:o+b], m)
+		i.block.Decrypt(dst[o:o+b], dst[o:o+b])
+		xor(dst[o:o+b], dst[o:o+b], c)
+
+		m = dst[o : o+b]
+		c = t
+	}
+}
--- ige.go
+++ ige.go
@@ -25,65 +25,6 @@ func newIGE(b cipher.Block, iv []byte) *ige {
 	return i
 }
 
-type igeEncrypter ige
-
-func (i *igeEncrypter) BlockSize() int {
-	return i.block.BlockSize()
-}
-
-func (i *igeEncrypter) CryptBlocks(dst, src []byte) {
-	if len(src)%i.block.BlockSize() != 0 {
-		panic("src not full blocks")
-	}
-	if len(dst) < len(src) {
-		panic("len(dst) < len(src")
-	}
-
-	b := i.block.BlockSize()
-	c := i.iv[:b]
-	m := i.iv[b:]
-
-	for o := 0; o < len(src); o += b {
-		xor(dst[o:o+b], src[o:o+b], c)
-		i.block.Encrypt(dst[o:o+b], dst[o:o+b])
-		xor(dst[o:o+b], dst[o:o+b], m)
-
-		c = dst[o : o+b]
-		m = src[o : o+b]
-	}
-}
-
-type igeDecrypter ige
-
-func (i *igeDecrypter) BlockSize() int {
-	return i.block.BlockSize()
-}
-
-func (i *igeDecrypter) CryptBlocks(dst, src []byte) {
-	if len(src)%i.block.BlockSize() != 0 {
-		panic("src not full blocks")
-	}
-	if len(dst) < len(src) {
-		panic("len(dst) < len(src)")
-	}
-
-	b := i.block.BlockSize()
-	c := i.iv[:b]
-	m := i.iv[b:]
-	t := make([]byte, b)
-
-	for o := 0; o < len(src); o += b {
-		t = src[o : o+b]
-
-		xor(dst[o:o+b], src[o:o+b], m)
-		i.block.Decrypt(dst[o:o+b], dst[o:o+b])
-		xor(dst[o:o+b], dst[o:o+b], c)
-
-		m = dst[o : o+b]
-		c = t
-	}
-}
-
 func checkIV(b cipher.Block, iv []byte) error {
 	// the initialization vector needs to contain b.Blocksize()*2 bytes because
 	// ige uses a two step xor process, and iv[:16] corresponds to the first iv
@@ -97,20 +38,4 @@ func checkIV(b cipher.Block, iv []byte) error {
 	}
 
 	return nil
-}
-
-func NewIGEEncrypter(b cipher.Block, iv []byte) IGE {
-	if err := checkIV(b, iv); err != nil {
-		panic(err.Error())
-	}
-
-	return (*igeEncrypter)(newIGE(b, iv))
-}
-
-func NewIGEDecrypter(b cipher.Block, iv []byte) IGE {
-	if err := checkIV(b, iv); err != nil {
-		panic(err.Error())
-	}
-
-	return (*igeDecrypter)(newIGE(b, iv))
 }
